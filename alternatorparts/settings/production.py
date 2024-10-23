e1bda3747608d11614381722e7884381e02a3ae9
Drew DuBois <andross@gmail.com>
2024-10-23 12:45:31 -0400

    manually added newsite.alternatorparts.com to csrf allowed sites

--- alternatorparts/settings/production.py
+++ alternatorparts/settings/production.py
@@ -22,7 +22,7 @@ if "alternatorparts" not in INSTALLED_APPS:
 CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
 if CLOUDRUN_SERVICE_URL:
     ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
-    CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
+    CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL, "https://newsite.alternatorparts.com"]
 else:
     ALLOWED_HOSTS = ["*"]
 
